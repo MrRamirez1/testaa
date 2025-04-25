7919adc0893acc8dc91c9ca06bdad2475817fcbc
MrRamirez1 <56929210+MrRamirez1@users.noreply.github.com>
2025-04-25 08:49:56 +0600

    Update save_data.php

--- 5work/save_data.php
+++ 5work/save_data.php
@@ -1,5 +1,5 @@
 <?php
-// Получаем данные из формы
+
 $name = $_POST['name'];
 $email = $_POST['email'];
 $age = $_POST['age'];
@@ -8,15 +8,15 @@ $interests = isset($_POST['interests']) ? implode(", ", $_POST['interests']) : '
 $activity = $_POST['activity'];
 $comments = $_POST['comments'];
 
-// Формируем строку для записи
+
 $data = "Имя: $name\nEmail: $email\nВозраст: $age\nПол: $gender\nИнтересы: $interests\nРод деятельности: $activity\nКомментарии: $comments\n\n";
 
-// Путь к файлу для записи данных
+
 $file = 'user_data.txt';
 
-// Открываем файл для добавления (если файл не существует, он будет создан)
+
 file_put_contents($file, $data, FILE_APPEND);
 
-// Перенаправляем пользователя на страницу с благодарностью или другой контент
+
 echo "Спасибо за отправку анкеты!";
 ?>
